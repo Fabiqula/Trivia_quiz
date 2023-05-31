a77b16fca3676c8130b9f8ee811111148626d0a7
Grzegorz Kowalczyk <gregorzkowalczykk@gmail.com>
2023-05-31 14:57:08 +0200

    Commit 2

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from question_model import Questions
+from data import question_data
+
+question_lib = []
+for questions in question_data:
+    question_text = questions["question"]
+
+    question_answer = questions["correct_answer"]
+
+    new_question = Questions(question_text, question_answer)
+    question_lib.append(new_question)
 
 
